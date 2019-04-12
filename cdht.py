215906acf016b9cf687e4de0792e0c43492753c8
Toby HUANG <z5141448@ad.unsw.edu.au>
2019-04-12 16:21:45 +1000

    simple peer manage system

--- cdht.py
+++ cdht.py
@@ -3,6 +3,7 @@
 from ping import UdpClient, UdpServer
 from store import Store
 from threading import Timer
+from peer import Peer
 
 
 def debug_print():
@@ -11,12 +12,25 @@ def debug_print():
 
 
 class Controller(object):
-    def __init__(self,my_id, peer):
-        # get the peer list by order 
-        peer =  [t for t in peer if t > my_id] +
-            [t+256 for t in peer if t < my_id]
+    def __init__(self,my_id, peer_ids ):
+        # set up the peer object 
+        self.peer = Peer(my_id)
+        [self.peer.add_suc(t) for t in peer_ids]
+        
+        # star the ping client 
+        for i in range(2):
+            self.start_ping_client(i)
+
+        # start up this ping server to accept pign 
+        self.start_pign_ser()
 
 
+    def start_ping_client(self, index):
+        # wrap and start the client 
+        UdpClient(self.peer.get_suc(index)).start()
+        
+    def start_pign_ser(self):
+        UdpServer().start()
 
 
 if __name__ == "__main__":
@@ -27,27 +41,18 @@ if __name__ == "__main__":
     Set up the arguements 
     """
     Store()['my_id'] = int(sys.argv[1])
-    peer_1 = int(sys.argv[2])
-    peer_2 = int(sys.argv[3])
     Store()['MSS'] = float(sys.argv[4])
     Store()['LOSS_RATE'] = float(sys.argv[5])
-    # worker array 
-    workers = []
 
+
+    """
+    Start up the controller 
+    """
+    Store()['controller'] =\
+        Controller(Store()['my_id'],[
+            int(sys.argv[2]),
+            int(sys.argv[3])
+        ])
+
+    # start up the debug mode 
     debug_print()
-
-    
-    """
-    Set up the environment for each thread 
-    """
-    # This client need to ping these server 
-    workers.append(UdpClient(peer_1))
-    workers.append(UdpClient(peer_2))
-    # start this server 
-    workers.append(UdpServer())
-
-    """
-    Run it!
-    """
-    for w in workers:
-        w.start()
