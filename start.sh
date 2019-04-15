853d4563300bb6f1eebc87684f16eeb8b9bd442c
Toby HUANG <619058900@qq.com>
2019-04-15 22:42:06 +1000

    modify the startup script

--- start.sh
+++ start.sh
@@ -10,11 +10,11 @@
 # tmux split-window -h -t 4 "python3 cdht.py  8 10 12 300 0.3"
 # tmux a 
 
-gnome-terminal  -t "Peer 1" -- python3  cdht.py 1 3 4 300 0.3 &
-gnome-terminal  -t "Peer 3" -- python3  cdht.py  3 4 5 300 0.3 &
-gnome-terminal  -t "Peer 4" -- python3  cdht.py  4 5 8 300 0.3 &
-gnome-terminal  -t "Peer 5" -- python3  cdht.py  5 8 10 300 0.3 &
-gnome-terminal  -t "Peer 8" -- python3  cdht.py  8 10 12 300 0.3 &
-gnome-terminal  -t "Peer 10" -- python3 cdht.py  10 12 15 300 0.3 &
-gnome-terminal  -t "Peer 12" -- python3 cdht.py  12 15 1 300 0.3 &
-gnome-terminal  -t "Peer 15" -- python3 cdht.py  15 1 3 300 0.3 &
+gnome-terminal  -t "Peer 1" -- bash -c 'python3  cdht.py 1 3 4 300 0.3; exec /bin/bash -i'  &
+gnome-terminal  -t "Peer 3" -- bash -c 'python3  cdht.py  3 4 5 300 0.3; exec /bin/bash -i'  &
+gnome-terminal  -t "Peer 4" -- bash -c 'python3  cdht.py  4 5 8 300 0.3; exec /bin/bash -i'  &
+gnome-terminal  -t "Peer 5" -- bash -c 'python3  cdht.py  5 8 10 300 0.3; exec /bin/bash -i'  &
+gnome-terminal  -t "Peer 8" -- bash -c 'python3  cdht.py  8 10 12 300 0.3; exec /bin/bash -i'  &
+gnome-terminal  -t "Peer 10" -- bash -c 'python3 cdht.py  10 12 15 300 0.3; exec /bin/bash -i'  &
+gnome-terminal  -t "Peer 12" -- bash -c 'python3 cdht.py  12 15 1 300 0.3; exec /bin/bash -i'  &
+gnome-terminal  -t "Peer 15" -- bash -c 'python3 cdht.py  15 1 3 300 0.3; exec /bin/bash -i'  &
